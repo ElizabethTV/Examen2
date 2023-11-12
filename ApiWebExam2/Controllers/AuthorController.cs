57081111b6e95023e71cef231816bf4c8402e217
Luz Elizabeth Velázquez Tovar <148408341+ElizabethTV@users.noreply.github.com>
2023-11-12 17:59:08 -0600

    Add files via upload

---
+++ ApiWebExam2/Controllers/AuthorController.cs
@@ -0,0 +1,29 @@
+﻿using exam2.Operaciones;
+using exam2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace ApiWebExam2.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private AuthorDAO authorDAO = new AuthorDAO();
+
+        //EndPoint para agregar nuevo autor
+        [HttpPost("author")]
+        public bool newAuthor([FromBody] Author author)
+        {
+            return authorDAO.newAuthor(author.Name);
+        }
+
+        //EndPoint para obtener autores
+        [HttpGet("authors")]
+        public List<Author> getAuthors()
+        {
+            return authorDAO.getAuthors();
+        }
+    }
+}
---
+++ ApiWebExam2/Controllers/BookController.cs
@@ -0,0 +1,52 @@
+﻿using exam2.Operaciones;
+using exam2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace ApiWebExam2.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDAO bookDAO = new BookDAO();
+
+        //EndPoint para agregar nuevo libro
+        [HttpPost("book")]
+        public bool newBook([FromBody] Book book)
+        {
+            return bookDAO.newBook(book.IdAuthor, book.Title, book.Chapters, book.Pages, book.Price);
+        }
+
+        //EndPoint para obtener los libros
+        [HttpGet("books")]
+        public List<Book> getBooks()
+        {
+            return bookDAO.getBooks();
+        }
+
+        //EndPoint para obtener libro por id
+        [HttpGet("book")]
+        public Book getBook(int idBook)
+        {
+            return bookDAO.getBook(idBook);
+        }
+
+        //EndPoint para obtener libro con author
+        [HttpGet("bookAuthor")]
+        public List<BookAuthor> getBookAuthor()
+        {
+            return bookDAO.getBookAuthor();
+        }
+
+        //EndPoint para buscar un libro por su titulo
+        [HttpGet("searchBook")]
+        public BookAuthor getsearchBook(string titleBook)
+        {
+            return bookDAO.getsearchBook(titleBook);
+        }
+    }
+}
+
+
